119344a72c5a63bfd8051c96e84eb1eaab873525
David Harket <davidharket123@gmail.com>
2022-10-30 20:12:22 +0100

    Initiall commit

---
+++ mail.py
@@ -0,0 +1,45 @@
+import smtplib
+from email.mime.text import MIMEText
+
+class Message:
+    def __init__(self):
+        self.my_email = "leienboks@gmail.com"
+        self.my_password = "xxpqaggtidzzengj"
+
+    def send_self(self, email, days, date, address, message):
+        print(email, days, date, address, message)
+
+        print(email, days, date, address, message)
+        with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+            connection.starttls()
+            connection.login(user=self.my_email, password=self.my_password)
+            connection.sendmail(
+                 from_addr=self.my_email,
+                 to_addrs=self.my_email,
+                 msg=f"Subject: Melding Fra:\n\n"
+                     f" E-post: {email}\n\n"
+                     f" Antall dager: {days}\n"
+                     f" Fra denne datoen: {date}\n"
+                     f" Til denne addressen: {address}\n"
+                     f" Ekstra beskjed: {message}"
+            )
+
+    def send_cus(self, email, days, date, address, message):
+        print(email, days, date, address, message)
+        print(email, days, date, address, message)
+        with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+            connection.starttls()
+            print(email, days, date, address, message)
+            connection.login(user=self.my_email, password=self.my_password)
+            content = f"Subject: Takk for din bestilling!:\n\n"\
+                      f"En kundebehandler kontakter deg straks.\n\n"\
+                      f"Informasjon om bestillingen\n"\
+                      f"Antall dager: {days}\n"\
+                      f"Fra denne datoen: {date}\n"\
+                      f"Til denne addressen: {address}\n"\
+                      f"Ekstra beskjed:{message}\n"
+            connection.sendmail(
+                 from_addr=self.my_email,
+                 to_addrs=f"{email}",
+                 msg=content
+            )
---
+++ main.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template, request
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, DateField, SelectField
+from wtforms.validators import DataRequired, URL, Email
+from mail import Message
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'Sodeffjed179'
+Bootstrap(app)
+
+class Leieform(FlaskForm):
+    email = StringField('Epost', validators=[DataRequired(), Email()])
+    days = SelectField('Hvor mange døgn vil du leie?', choices=["-", "1", "2", "3"], validators=[DataRequired()])
+    date = DateField('Hvilken dato vil du leie fra?', format='%d-%m-%Y')
+    address = StringField('Hvilken addresse skal vi levere til?', validators=[DataRequired()])
+    submit = SubmitField("Bestill")
+
+
+@app.route('/', methods=["POST", "GET"])
+def home():
+    form = Leieform()
+    if request.method == "GET":
+        email = None
+        days = None
+        date = None
+        address = None
+        message = None
+        return render_template("index.html", form=form)
+    else:
+        email = None
+        days = None
+        date = None
+        address = None
+        message = None
+        if request.method == "POST":
+            email = request.form["email"]
+            days = request.form["days"]
+            date = request.form["date"]
+            address = request.form["address"]
+            message = request.form["message"]
+            mail = Message()
+            mail.send_self(email, days, date, address, message)
+            mail.send_cus(email, days, date, address, message)
+            return render_template("index.html", form=form, email=email, days=days,
+                                   date=date, address=address, message=message)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
