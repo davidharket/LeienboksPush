f31e3c8db76abdcd20480f6d2f032fecdab621e8
David Harket <davidharket123@gmail.com>
2022-10-31 13:30:48 +0100

    fix .env (probably final)

--- mail.py
+++ mail.py
@@ -1,11 +1,9 @@
 import smtplib
-import dotenv
-import os
-dotenv.load_dotenv()
+import config
 class Message:
     def __init__(self):
         self.my_email = "leienboks@gmail.com"
-        self.my_password = os.getenv("APP_PASSWORD")
+        self.my_password = config.APP_PASSWORD
 
     def send_self(self, email, days, date, address, message):
         print(f"your password is :{self.my_password}")
